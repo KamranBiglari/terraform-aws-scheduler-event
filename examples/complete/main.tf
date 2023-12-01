0fc9df2a4d8c964e06366c2bc84e07912aac0387
Kamran Biglari <kamran.biglari@newchangefx.com>
2023-12-01 15:21:30 +0000

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,20 +1,5 @@
-terraform {
-  required_version = ">= 0.13.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region  = "eu-west-2"
-  profile = "ncfx-dev"
-}
-
 module "scheduler_ecs_control" {
-    source  = "../"
+    source  = "../../"
     name_prefix    = "ecs-servicecontrol"
     group_name = "default"
 
